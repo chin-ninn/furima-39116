7b4b4d398d0b18d4df682dd3cfe6f4128b4361bc
chin-ninn <standbloom1220@gmail.com>
2023-07-02 21:01:35 +0900

    ユーザー管理機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,            presence: true
-  validates :family_name,         presence: true
-  validates :first_name,          presence: true
-  validates :family_name_reading, presence: true
-  validates :first_name_reading,  presence: true
   validates :birth,               presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,7 +52,14 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include 'Password is too short (minimum is 6 characters)'
       end
 
-      it 'passwordが半角英数字が混合していないと登録できない' do
+      it 'passwordが半角英字のみでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'Password Password include both letters and numbers in password'
+      end
+
+      it 'passwordが半角数字のみでは登録できない' do
         @user.password = '000000'
         @user.password_confirmation = '000000'
         @user.valid?
@@ -78,14 +85,14 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include "First name can't be blank"
       end
 
-      it 'family_nameが半角だと登録できない' do
-        @user.family_name = 'aaaa'
+      it 'family_nameに半角英字、全角英字、半角カタカナが含まれると登録できない' do
+        @user.family_name = 'ｙあmaﾀﾞ'
         @user.valid?
         expect(@user.errors.full_messages).to include 'Family name Use double-byte characters'
       end
 
-      it 'first_nameが半角だと登録できない' do
-        @user.first_name = 'aaaa'
+      it 'first_nameに半角英字、全角英字、半角カタカナが含まれると登録できない' do
+        @user.first_name = 'ｔあroｳ'
         @user.valid?
         expect(@user.errors.full_messages).to include 'First name Use double-byte characters'
       end
@@ -102,14 +109,14 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include "First name reading can't be blank"
       end
 
-      it 'family_name_readingが全角（カタカナ）でないと登録できない' do
-        @user.family_name_reading = '山田'
+      it 'family_name_readingに全角漢字、全角ひらがな、半角カタカナが含まれると登録できない' do
+        @user.family_name_reading = 'やﾏ田'
         @user.valid?
         expect(@user.errors.full_messages).to include 'Family name reading Use double-byte kana'
       end
 
-      it 'first_name_readingが全角（カタカナ）でないと登録できない' do
-        @user.first_name_reading = '太郎'
+      it 'first_name_readingに全角漢字、全角ひらがな、半角カタカナが含まれると登録できない' do
+        @user.first_name_reading = '太ろｳ'
         @user.valid?
         expect(@user.errors.full_messages).to include 'First name reading Use double-byte kana'
       end
