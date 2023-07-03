84cb61bdb3d94ee032144a3e2e8e8f6b5754eec9
chin-ninn <standbloom1220@gmail.com>
2023-07-03 20:48:37 +0900

    itemモデル、itemsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_25_231559) do
+ActiveRecord::Schema.define(version: 2023_07_03_112714) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "item_text", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "charges_id", null: false
+    t.integer "area_id", null: false
+    t.integer "days_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 2023_06_25_231559) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
