04b523cffaca7c2c093a7f550f222ba9fc96c18c
chin-ninn <standbloom1220@gmail.com>
2023-07-09 18:43:59 +0900

    商品出品ページの表示・情報の保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,15 +5,15 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :item_text, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
-  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
-  validates :charges_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
-  validates :area_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
-  validates :days_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :charges_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :days_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :price, presence: true
   validates :image, presence: true
 
-  extend ActiveHash::Association::ActiveRecordExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :charges
