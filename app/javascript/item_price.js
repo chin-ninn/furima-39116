3d5456606f32754dd525d44aa2ad3ef83f9739ed
chin-ninn <standbloom1220@gmail.com>
2023-07-10 10:56:41 +0900

    販売手数料と販売利益の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener("input", () => {
+    const priceValue = itemPrice.value;
+
+    const itemFee = document.getElementById("add-tax-price");
+    itemFee.innerHTML = Math.floor(priceValue * 0.1);
+    console.log(itemFee.innerHTML);
+
+    const itemProfit = document.getElementById("profit");
+    itemProfit.innerHTML = Math.floor(priceValue - itemFee.innerHTML);
+    console.log(itemProfit.innerHTML);
+  });
+});
