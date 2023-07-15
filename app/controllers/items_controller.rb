52e7f95ce4974114742472e92ce644af6c18bccf
chin-ninn <standbloom1220@gmail.com>
2023-07-16 08:22:23 +0900

    詳細ページの表示の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_item, only: :show
   def index
     @items = Item.all.order(created_at: "DESC")
   end
@@ -17,9 +18,16 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :item_text, :category_id, :condition_id, :charges_id, :area_id, :scheduled_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
