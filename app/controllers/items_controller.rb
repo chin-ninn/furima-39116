291ee35d360e34a6fd7c1d1402048538203a2935
chin-ninn <standbloom1220@gmail.com>
2023-07-23 21:30:05 +0900

    商品削除機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, except: [:index, :show]
 
   def index
@@ -35,6 +35,13 @@ class ItemsController < ApplicationController
       redirect_to action: :show
     else
       render action: :edit
+    end
+  end
+
+  def destroy
+    if current_user.id == @item.user_id
+      @item.destroy
+      redirect_to action: :index
     end
   end
 
